341ab6cbe11178b9e28b644b623c583f093c9dc3
Parth Pandya <parth@net4nuts.com>
2015-08-12 15:31:52 +0530

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Parth Pandya on 12/08/15.
+//  Copyright (c) 2015 Parth Pandya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
